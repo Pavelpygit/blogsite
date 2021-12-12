c0376789310c952611915e020f800972e2a357bd
Pavel <pavelavgustinovic4@gmail.com>
2021-12-12 23:34:18 +0300

    com2. style and html

--- blogsite/article/models.py
+++ blogsite/article/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Article(models.Model):
-    author = models.ForeignKey('auth.User', on_delete = models.CASCADE,)
-    title = models.CharField(max_length = 100)
-    body = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete = models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length = 100, verbose_name='Тема')
+    body = models.TextField(verbose_name='Текст')
     def __str__(self):
         return self.title
