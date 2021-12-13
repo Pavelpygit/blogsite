5e1a2715bc5a3603275f9c2f7f802b76c4540d2c
Pavel <pavelavgustinovic4@gmail.com>
2021-12-13 22:33:50 +0300

    the end

--- blogsite/article/models.py
+++ blogsite/article/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Article(models.Model):
-    author = models.ForeignKey('auth.User', on_delete = models.CASCADE, verbose_name='Автор')
-    title = models.CharField(max_length = 100, verbose_name='Тема')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=100, verbose_name='Тема')
     body = models.TextField(verbose_name='Текст')
+
     def __str__(self):
         return self.title
